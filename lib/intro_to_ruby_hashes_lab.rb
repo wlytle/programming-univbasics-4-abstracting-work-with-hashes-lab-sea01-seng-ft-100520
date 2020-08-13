e2781695c8e748b180c2d08e4e75b051db19c1e5
Will Lytle <wlytle89@gmail.com>
2020-08-13 19:01:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,8 +2,7 @@ require 'pry'
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash = {}
-  hash[key] = value
+  { key => value }
 end
 
 def read_from_hash(hash, key)
